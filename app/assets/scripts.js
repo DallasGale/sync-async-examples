fc4e3edbc9a7cc23910aa549d65b9788d6d631d5
Dallas Gale <info@eyespynature.net>
2018-02-05 08:10:59 +1100

    wip

--- app/assets/scripts.js
+++ app/assets/scripts.js
@@ -1,75 +1,126 @@
-(test = () => {
-  console.log('here');
-})();
+
+ 
+  (test = () => {
+    console.log('here');
+  })();
 
 
-/////////////////////////////// XMLHttpRequest() ///////////////////////////////
+  // XMLHttpRequest() 
 
-// async example
-const xhr = new XMLHttpRequest();
-      xhr.open('GET', 'docs/hello.txt', true); // true signifies that the request is to be handled asynchronous
-      // xhr.send();
-      xhr.onload = function(e) {
-        if ( xhr.readyState === 4 ) { //4 means complete
-          console.log('xhr.readyState = ' + xhr.readyState);
-          if ( xhr.status === 200 ) {
-            console.log('xhr.status = ' + xhr.status);
-          } else {
-            console.error('xhr.statusText = ' + xhr.statusText);
-          }
+  // async example
+  xmlExample = () => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'docs/hello.txt', true); // true signifies that the request is to be handled asynchronous
+    // xhr.send();
+    xhr.onload = (e) => {
+      if ( xhr.readyState === 4 ) { //4 means complete
+        console.log('xhr.readyState = ' + xhr.readyState);
+        if ( xhr.status === 200 ) {
+          console.log('xhr.status = ' + xhr.status);
+        } else {
+          console.error('xhr.statusText = ' + xhr.statusText);
         }
-        console.log('Asynchronous hello.txt: ', this.response);
-      };
-      xhr.onerror = function (e) {
-        console.error(xhr.statusText);
-      };
-      xhr.send(); // initiates the request
+      }
+      console.log('Asynchronous hello.txt: ', this.response);
+    };
+    xhr.onerror = (e) => {
+      console.error(xhr.statusText);
+    };
+    xhr.send(); // initiates the request
+  }
 
-/////////////////////////////// XMLHttpRequest() ///////////////////////////////
+  /////////////////////////////// XMLHttpRequest() ///////////////////////////////
 
-function xhrSuccess() {
-  this.callback.apply(this, this.arguments);
-}
+  // xhrSuccess = () => {
+  //   this.callback.apply(this, this.arguments);
+  // }
 
-function xhrError() {
-  console.error(this.statusText);
-}
+  // xhrError = () => {
+  //   console.error(this.statusText);
+  // }
 
-function showMessage(message) {
-  console.log(message + this.responseText);
-}
+  // showMessage = (message) => {
+  //   console.log(message + this.responseText);
+  // }
 
-function loadFile(url, callback) { // hello.txt, showMessage() //
-  var xhr = new XMLHttpRequest();
-      xhr.callback = callback;
-      xhr.arguments = Array.prototype.slice.call(arguments, 2);
-      xhr.onload = xhrSuccess;
-      xhr.onerror = xhrError;
-      xhr.open('GET', url, true);
-      xhr.send();
-}
+  // loadFile = (url, callback) => { // hello.txt, showMessage() //
+  //   var xhr = new XMLHttpRequest();
+  //       xhr.callback = callback;
+  //       xhr.arguments = Array.prototype.slice.call(arguments, 2);
+  //       xhr.onload = xhrSuccess;
+  //       xhr.onerror = xhrError;
+  //       xhr.open('GET', url, true);
+  //       xhr.send();
+  // }
 
-loadFile('docs/hello.txt', showMessage, 'New message!\n\n');
-
-
+  // loadFile('docs/hello.txt', showMessage, 'New message!\n\n');
 
 
-//////////////////////////////////// fetch() ///////////////////////////////////
 
-function delay(ms) {
-    return new Promise(function(resolve) {
-        setTimeout(resolve, ms);
-    });
-}
 
-async function asyncAwait() {
-  console.log('1');
+  /// asyncAwait(); 
 
-  await delay(1000);
-  console.log('2');
+  delay = (ms) => {
+      return new Promise(function(resolve) {
+          setTimeout(resolve, ms);
+      });
+  }
 
-  await delay(1000);
-  console.log('3');
-}
+  async function asyncAwait() {
+    console.log('1');
 
-asyncAwait();
+    await delay(1000);
+    console.log('2');
+
+    await delay(1000);
+    console.log('3');
+  }
+
+
+  // setTimeout()
+
+  async function helloGoodBye() {
+    console.log('Hello');
+    setTimeout( () => {
+      console.log('Goodbye!');
+    }, 2000);
+    await delay(1800);
+    console.log('Hello Again');  
+  }
+
+
+  // Ajax request
+  // $(function() {
+  //     function getData(callback) { 
+  //       $.get('https://jsonplaceholder.typicode.com/posts/1', callback);
+  //     }
+
+  //     var data = getData();
+  //     console.log(data);
+  //   });
+
+
+  greeting = (name) => {
+    alert('Hello ' + name);
+  }
+
+  processUserInput = (callback) => {
+      let name = prompt('Please enter your name.');
+      callback(name);
+  }
+
+  processUserInput(greeting);
+
+
+        
+      
+      
+
+
+
+
+  //---------- 
+
+  // helloGoodBye();
+  // asyncAwait();
+  // xmlExample();
